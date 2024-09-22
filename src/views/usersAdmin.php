f45c5f1ef4abeb9c0b80bf65851587b79ef2d294
zesper1 <zxdxsprx@gmail.com>
2024-09-22 09:58:37 +0800

    Update usersAdmin.php

--- src/views/usersAdmin.php
+++ src/views/usersAdmin.php
@@ -432,22 +432,22 @@ labeli{
     
     <div class="items">
         <line>
-            <a href="dashboardAdmin.php"><img src="../PHPLOGIN/images/dashboard.png" class="logos"></a>
+            <a href="dashboardAdmin.php"><img src="../../public/assets/images/dashboard.png" class="logos"></a>
             <label> DASHBOARD</label>
         </line>
         
         <line>
-            <a href="reportsAdmin.php"><img src="../PHPLOGIN/images/report.png" class="logos"></a>
+            <a href="reportsAdmin.php"><img src="../../public/assets/images/report.png" class="logos"></a>
             <label> REPORTS</label>
         </line>
         
         <line>
-            <a href="appealAdmin.php"><img src="../PHPLOGIN/images/paper.png" class="logos"></a>
+            <a href="appealAdmin.php"><img src="../../public/assets/images/paper.png" class="logos"></a>
             <label> REPLY TO APPEAL</label>
         </line>
         
         <line>
-            <a href="usersAdmin.php"><img src="../PHPLOGIN/images/users.png" class="logos"></a>
+            <a href="usersAdmin.php"><img src="../../public/assets/images/users.png" class="logos"></a>
             <label> VIEW USER</label>
         </line>
     </div>
@@ -456,12 +456,12 @@ labeli{
         <div class="wrapper">
             <div class="box2">
                 <div class="info">
-                    <img src="../PHPLOGIN/images/logout.png" class="toplogo">
+                    <img src="../../public/assets/images/logout.png" class="toplogo">
                     <label class="name ">NAME</label>
                 </div>                
                 <div class="info2">
-                    <img src="../PHPLOGIN/images/bell.png" class="toplogo">
-                    <img src="../PHPLOGIN/images/settings.png" class="toplogo">
+                    <img src="../../public/assets/images/bell.png" class="toplogo">
+                    <img src="../../public/assets/images/settings.png" class="toplogo">
                 </div>
             </div>
 
@@ -476,7 +476,7 @@ labeli{
 
             <div class="col">
     <button id="addBtn"> 
-        <img src="../PHPLOGIN/images/plus-5-xl.png" class="plus"> 
+        <img src="../../public/assets/images/plus-5-xl.png" class="plus"> 
     </button>
     <div class="input-box1">
         <select id="roleSelect" name="role" placeholder="Select a role">             
